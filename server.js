0a3e87549ab76ff8e59258aad7709d6e6aeff9d2
Jay Kapoor <jkap86@gmail.com>
2023-10-09 14:05:40 -0400

    -

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('*', (req, res) => {
+    res.redirect(301, 'https://sleepier-okarq.ondigitalocean.app/');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on ${PORT}`);
+});
