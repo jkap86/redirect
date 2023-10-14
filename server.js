8febc920b12145d26c931d89918bdeddbbc555a6
Jay Kapoor <jkap86@gmail.com>
2023-10-13 20:40:44 -0400

    -

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const https = require('https');
 
-
+setInterval(() => {
+    console.log(`Pinging server at ${new Date()}`)
+    https.get('https://sleepier.herokuapp.com/')
+}, 25 * 60 * 1000);
 
 app.get('*', (req, res) => {
     res.redirect(301, `https://sleepier-okarq.ondigitalocean.app${req.originalUrl}`);
